faff94d1e1125bfb6568fd4c9ae4e33841276c62
rigor789 <rigor789@gmail.com>
2017-10-08 20:58:14 +0100

    Push back to the source repo from a different directory (using a GITHUB_TOKEN)

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+cd $HOME
+
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
+git clone --quiet --branch=master https://${GITHUB_TOKEN}@github.com/rigor789/nativescript-vue-ui-tests.git deploy > /dev/null 2>&1
+
+cd deploy
+cp -R $TRAVIS_BUILD_DIR/screenshots .
+
 git add -f screenshots
-git commit --message "Travis deploy: $TRAVIS_JOB_NUMBER"
+git commit --message "[Travis Screenshots: $TRAVIS_JOB_NUMBER] [skip ci]"
 
-git remote add origin-push https://${GITHUB_TOKEN}@github.com/rigor789/nativescript-vue-ui-tests.git > /dev/null 2>&1
-
-git pull origin-push master
-
-git push origin-push master
+git pull --force origin master
+git push --force origin master
