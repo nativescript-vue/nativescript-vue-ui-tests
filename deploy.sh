990a12c23eabb8474aacaee61ed3c26c4a1537bb
rigor789 <rigor789@gmail.com>
2017-10-08 20:32:59 +0100

    Change deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout -b gh-pages
 git add -f screenshots
 git commit --message "Travis deploy: $TRAVIS_JOB_NUMBER"
 
-git remote add origin-pages https://${GITHUB_TOKEN}@github.com/rigor789/nativescript-vue-ui-tests.git > /dev/null 2>&1
-git push --force origin-pages gh-pages
+git remote add origin-push https://${GITHUB_TOKEN}@github.com/rigor789/nativescript-vue-ui-tests.git > /dev/null 2>&1
+
+git pull origin-push master
+
+git push origin-push master
